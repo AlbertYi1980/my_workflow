212c052510ec9c14c6491e1b3ac218855fb81473
albert <yicheng@cxist.com>
2020-12-14 11:13:16 +0800

    workflow def

---
+++ WorkflowSample/MyWorkflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Linq;
+
+namespace MyWorkflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Activity workflow1 = new Workflow1();
+            WorkflowInvoker.Invoke(workflow1);
+            Console.WriteLine("enter to end..");
+            Console.ReadLine();
+        }
+    }
+
+    public class A
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public B b { get; set; }
+    }
+
+    public class B
+    {
+        public int Count { get; set; }
+    }
+}
---
+++ WorkflowSample/WorkflowHost/Database/WorkflowDefinitionContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using System.Data.SqlServerCe;
+
+namespace WorkflowHost.Database
+{
+    public class WorkflowDefinitionContext : DbContext
+    {
+        public WorkflowDefinitionContext(): 
+            base(new SqlCeConnection("Data Source=e:\\MyDatabase.sdf;Persist Security Info=False;"), contextOwnsConnection: true)
+        {
+
+        }
+        public DbSet<WorkflowDefinition> WorkflowDefinitions { get; set; }
+
+    }
+
+}
